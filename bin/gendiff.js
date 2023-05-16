e4169657e3dfc8b1ec438993ded35b4db56949eb
Your Name <you@example.com>
2023-05-16 22:44:26 +0500

    absolute way

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import getObject from '../src/index.js';
 
 program
 .version('1.0.0')
@@ -10,4 +11,7 @@ program
 .helpOption('-h, --help', 'output usage information')
 .option('-f, --format <type>', 'output format', 'stylish')
 .arguments('<filepath1> <filepath2>')
-program.parse(process.argv);
+.action((filepath1, filepath2)=> {
+console.log(getObject(filepath1, filepath2))
+})
+program.parse();
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import _ from 'lodash'
+import fs from 'fs';
+import path from 'path';
+
+const getObject = (file1, file2) => {
+  
+
+  const filePath1 = path.isAbsolute(file1) ? file1 : path.resolve(process.cwd(), file1);
+  const filePath2 = path.isAbsolute(file2) ? file2 : path.resolve(process.cwd(), file2);
+
+  const data1 = JSON.parse(fs.readFileSync(filePath1, 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(filePath2, 'utf-8'));
+  
+  const objectUnion = _.union(Object.keys(data1), Object.keys(data2));
+  
+  const getDifference = objectUnion.map((key) => {
+    if (!_.has(data1, key)) {
+      return `- ${key}: ${data2[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `+ ${key}: ${data1[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return `- ${key}: ${data2[key]}\n+ ${key}: ${data1[key]}`;
+    } else { 
+      return `  ${key}: ${data1[key]}`;
+    }
+  });
+  
+  const sortedDifference = getDifference.sort((a, b) => {
+    const aKey = a.slice(2);
+    const bKey = b.slice(2);
+    return aKey < bKey ? -1 : aKey > bKey ? 1 : 0;
+  });
+  
+  return `{\n${sortedDifference.join('\n')}\n}`;
+};
+export default  getObject
