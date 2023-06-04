b1c0210de0e16a5b8a25e8f20ffbe62cbce3c17a
radmir <radmir@MSI>
2023-06-04 10:16:12 +0500

    fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,9 +12,9 @@ const plain = (diffTree) => {
     };
 
   const iter = (node, path = '') => node.flatMap((prop) => {
-  const {
-      key, type, value, previous, current, children,
-    } = prop;
+    const {
+    key, type, value, previous, current, children,
+  } = prop;
     const currentPath = [path, key].filter(Boolean).join('.');
     switch (type) {
       case 'added':
